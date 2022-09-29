e14c1fda91637ec5d761814c34213661258bf2e9
younglee99 <tldud6940@naver.com>
2022-09-29 23:10:42 +0900

    키워드 추가(최종)

--- src/page/KeywordPage.js
+++ src/page/KeywordPage.js
@@ -3,36 +3,38 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import '../style/page/KeywordPage.css';
 
-function Create(props){
-    return <article>
-        <form onSubmit={event=>{
-            event.preventDefault();
-            const keyword = event.target.keyword.value;
-            props.onCreate(keyword);
-        }}>
-            <div id="inp">
-                <input id="k_Text" type="text" name="keyword" ></input>
-                <input id="k_Add" type="submit" value="추가"></input>
-            </div>
-        </form>
-    </article>
-}
-
-
 
 const KeywordPage = () => {
-    const [topics, setTopics] = useState([]);
+    const [keyword, setKeyword] = useState('');
+    const [word, setWord]= useState('');
+    const onChange = (e) => {
+    setKeyword(e.target.value);
     
+    };
+    const onReset = (e) => {
+        setWord([
+            ...word,
+            keyword
+        ])
+        // setKeyword(''); 
+      }
+
+      console.log(word);
     return (
         <div className='page'>
             <Header title = "키워드 알림 설정"/>
             <div className='middle'>      
-                <Create onCreate={(keyword)=>{
-                    const newTopic = {keyword:keyword};
-                    const newTopics = [...topics];
-                    newTopics.push(newTopic);
-                    setTopics(newTopics);
-                }}></Create>
+                <input className="k_Text"
+                type="text"
+                placeholder="키워드를 입력하세요"
+                onChange={onChange}
+                value={keyword}
+                />
+            <button className="k_Add" onClick={onReset}>추가</button>
+            {`${word}`}
+            {word.map((e)=>{
+                return <div><div className=''>word</div>|<div onClick={delete}>X</div></div>
+            })}
             </div>
             <Footer/>
         </div>
